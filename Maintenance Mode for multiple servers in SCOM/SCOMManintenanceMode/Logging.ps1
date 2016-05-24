7baff6acea95407ed5f704398147b03b843c9eaf
Sandor Makai <Sandor.Makai@inframon.com>
2016-05-24 11:01:30 +0100

    Adding Logging.ps1 and Initial part of SCOMMaintenanceMode.ps1

---
+++ Maintenance Mode for multiple servers in SCOM/SCOMManintenanceMode/Logging.ps1
@@ -0,0 +1,38 @@
+﻿#Logger functions
+IF ((Test-Path variable:constERROR) -eq 0) {
+	Set-Variable -name constERROR -value 1 -option Constant
+	}
+IF ((Test-Path variable:constINFO) -eq 0) {
+	Set-Variable -name constINFO -value 2 -option Constant
+}
+IF ((Test-Path variable:constDATA) -eq 0) {
+	Set-Variable -name constDATA -value 3 -option Constant
+}
+
+function IsFileExist ($strFile)
+{
+	Return Test-Path -PathType Leaf -Path $strFile
+}
+
+function LogToFile ($intLogType, $strFile, $strLogData)
+{
+	switch ($intLogType)
+	{
+		1 {
+			$strDataToFile = "{0:MM}-{0:dd}-{0:yyyy} - {0:hh}:{0:mm}:{0:ss}" -f (Get-Date)
+			$strDataToFile = $strDataToFile + " --- ERROR --- " + $strLogData
+			Add-Content -Path $strFile -Value $strDataToFile
+		}
+		2 {
+			$strDataToFile = "{0:MM}-{0:dd}-{0:yyyy} - {0:hh}:{0:mm}:{0:ss}" -f (Get-Date)
+			$strDataToFile = $strDataToFile + " --- INFO --- " + $strLogData
+			Add-Content -Path $strFile -Value $strDataToFile
+		}
+		3 {
+			Add-Content -Path $strFile -Value $strLogData
+		}
+		default {
+			Return 10
+		}
+	}
+}
--- Maintenance Mode for multiple servers in SCOM/SCOMManintenanceMode/SCOMMaintenanceMode.ps1
+++ Maintenance Mode for multiple servers in SCOM/SCOMManintenanceMode/SCOMMaintenanceMode.ps1
@@ -1,4 +1,42 @@
 ﻿#
 # Script.ps1
 #
-Get-Module -ListAvailable | ? {$_.Name -eq "OperationsManager"} 
+###################### INIT #############################################################
+
+$strScriptPath = $NULL
+$strScriptName = $NULL
+$strLogFile = $NULL
+
+function Get-ScriptDirectory
+{
+	$Invocation = $NULL
+	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
+	If ($Invocation -eq $NULL) {
+		If ($strLogFile -eq $NULL) {
+			$strCommand = $ErrorReturn.InvocationInfo.Line
+			$strException = $ErrorReturn.Exception
+			Write-Host "Error occured during command: $strCommand `r`nThe exception of the error is: $strException"
+			Exit -1
+		}
+		Else {
+			$strCommand = $ErrorReturn.InvocationInfo.Line
+			$strException = $ErrorReturn.Exception
+			LogToFile -intLogType $constERROR -strFile $strLogFile -strLogData "Error occured during command: $strCommand"
+			LogToFile -intLogType $constDATA -strFile $strLogFile -strLogData "The exception of the error is: $strException"
+			$ReturnValue = $false
+			return
+		}
+		
+	}
+	Split-Path $Invocation.MyCommand.Path
+}
+
+.((Get-ScriptDirectory) + "\Logging.ps1")
+
+If (-not($strScriptPath = Get-ScriptDirectory)) {
+	Write-Host "The script cannot determinde its working directory. Without this parameter the script cannot run. Please check and run the script again!"
+	Exit -2
+}
+$strScriptName = ($MyInvocation.MyCommand.Name).Substring(0,($MyInvocation.MyCommand.Name).Length-4)
+$strLogFile = ($strScriptPath + "\" + $strScriptName + ".log")
+
